0f0602f0598ab8c7551f8c4f52a5caf92e88c69c
Kay <hiebertkay@gmail.com>
2023-11-19 14:27:41 -0500

    added more questions and answers and how they're displayed,  to script.js, set up start button

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,34 @@
-var questions = document.querySelector("#questions");
+var questionList = [
+    {
+        question: "Commonly used datas types DO NOT include:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ____________.",
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____________.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "String values must be enclosed within __________ when being assigned to variables.",
+        answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswerIndex: 3
+    }
+];
+
 var instructions = document.querySelector("#intro");
+var questions = document.querySelector("#questions");
+questions.setAttribute("style", "display: none;");
 
 var questionText = document.querySelector("#question-text");
 var answer1 = document.querySelector("#answer-1");
@@ -7,50 +36,20 @@ var answer2 = document.querySelector("#answer-2");
 var answer3 = document.querySelector("#answer-3");
 var answer4 = document.querySelector("#answer-4");
 
-var questions = [
-    {
-        question: "Commonly used datas types DO NOT include:",
-        answers: [
-            "1. strings", "2. booleans", "3. alerts", "4. numbers"
-        ]
-    },
-    {
-        question: "The condition in an if / else statement is enclosed with____________.",
-        answers: [
-            "1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"
-        ]
-    },
-    {
-        question: "Arrays in JavaScript can be used to store_____________.",
-        answers: [
-            "1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"
-        ]
-    },
-];
+var startButton = document.querySelector("#start-quiz");
+startButton.addEventListener("click", displayQuestions);
+displayQuestion(0);
+
+function displayQuestions() {
+    instructions.setAttribute("style", "display: none;");
+    questions.setAttribute("style", "");
+}
 
 function displayQuestion(questionNumber) {
-    var question = questions[questionNumber];
+    var question = questionList[questionNumber];
     questionText.textContent = question.question;
     answer1.textContent = question.answers[0];
     answer2.textContent = question.answers[1];
     answer3.textContent = question.answers[2];
     answer4.textContent = question.answers[3];
 }
-/*
-displayQuestionOne();
-
-function displayQuestionOne() {
-    questionText.textContent = "Commonly used datas types DO NOT include:";
-    answer1.textContent = "1. strings";
-    answer2.textContent = "2. booleans";
-    answer3.textContent = "3. alerts";
-    answer4.textContent = "4. numbers";
-
-}
-function displayQuestionTw0() {
-    questionText.textContent = "The condition in an if / else statement is enclosed with_________.:";
-    answer1.textContent = "1. quotes";
-    answer2.textContent = "2. curly brackets";
-    answer3.textContent = "3. parenthesis";
-    answer4.textContent = "4. square brackets";
-    */
